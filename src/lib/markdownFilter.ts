6fa4032e98e68ecd0f883907c8c21c8a4e6243da
tukumo <himagami126@gmail.com>
2019-03-30 20:52:03 +0900

    add: markdownFilter

---
+++ src/lib/markdownFilter.ts
@@ -0,0 +1,60 @@
+/**
+ * タグのブロックを返却する
+ *
+ * @export
+ * @param {string} markDown
+ * @param {string} selectHeader
+ * @returns
+ */
+export default function(markDown: string, selectHeader: string) {
+  let filteredMarkdown = "";
+  let isExtractionTarget = false;
+  let currentTagDepth = 1;
+
+  markDown.split("\n").forEach((line: string) => {
+    // タグの深さを取得
+    const tagDepth = headerTagDepth(line);
+    // 抽出しない条件
+    if (isExtractionTarget && (tagDepth > 0 && tagDepth <= currentTagDepth)) {
+      isExtractionTarget = false;
+    }
+
+    // 抽出する条件
+    if (!isExtractionTarget) {
+      // 対象のブロックか判定
+      if (tagDepth === -1 || line.indexOf(selectHeader) === -1) {
+        return;
+      }
+
+      // 抽出条件の設定
+      isExtractionTarget = true;
+      currentTagDepth = tagDepth;
+    }
+
+    // 抽出対象ならfilteredMarkdownに追記
+    if (isExtractionTarget) {
+      filteredMarkdown += line + "\n";
+    }
+  });
+  return filteredMarkdown;
+}
+
+/**
+ * タグの深さを測定し返す
+ * タグではないときは-1を返却
+ *
+ * @param {string} tags
+ */
+function headerTagDepth(tags: string): number {
+  for (let i = 0; i < tags.length; i++) {
+    switch (tags[i]) {
+      case "#":
+        continue;
+      case " ":
+        return i;
+      default:
+        return -1;
+    }
+  }
+  return -1;
+}
