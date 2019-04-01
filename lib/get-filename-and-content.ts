55b416e6106b4b79e106de1a120f2d25a4fbe18b
r-takada <r.takada@fenrir.co.jp>
2019-04-01 12:23:04 +0900

    add: ファイル名取得処理

---
+++ lib/get-filename-and-content.ts
@@ -0,0 +1,28 @@
+import { statSync, readdirSync, readFileSync } from "fs";
+import getFilenameAndContent from "./get-filename-and-content";
+
+/**
+ * fileの一覧と本文を取得
+ *
+ * @param {string} path
+ * @returns {{ title: string, content: string }[]}
+ */
+export default function (path: string): { title: string, content: string }[] {
+  const stat = statSync(path);
+
+  if (stat.isFile() && path.substr(-3) === ".md") {
+    return [{
+      title: path,
+      content: readFileSync(path, { encoding: "utf-8" })
+    }];
+  }
+
+  if (stat.isDirectory()) {
+    const fileNames: { title: string, content: string }[] = [];
+    readdirSync(path).forEach((childPath: string) =>
+      fileNames.push(...getFilenameAndContent(`${path}/${childPath}`))
+    );
+    return fileNames;
+  }
+  return [];
+}
