1d148698ecca4860e34eca110899cb24840859e3
tukumo <himagami126@gmail.com>
2019-04-04 00:25:45 +0900

    modify: 不要となったファイルの除去

--- build/lib/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var markdown_hash_filter_1 = require("./markdown-hash-filter");
-var get_filename_and_content_1 = require("./get-filename-and-content");
-module.exports = {
-    markdownFilter: markdown_hash_filter_1.markdownHashFilter,
-    getFilenameAndContent: get_filename_and_content_1.getFilenameAndContent
-};
-//# sourceMappingURL=index.js.map
