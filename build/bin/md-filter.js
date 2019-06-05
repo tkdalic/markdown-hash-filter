147631d2294d1637432bfbf4b2020be17e75fe8f
r-takada <r.takada@fenrir.co.jp>
2019-06-05 15:37:56 +0900

    modify: buildをgit管理下から削除

--- build/bin/md-filter.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("../src/index");
-//# sourceMappingURL=md-filter.js.map
--- build/lib/markdown-hash-filter.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * タグのブロックを返却する
- *
- * @export
- * @param {string} markDown
- * @param {string} selectHeader
- * @returns
- */
-export declare function markdownHashFilter(markDown: string, selectHeader: string): string;
